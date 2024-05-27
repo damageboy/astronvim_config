2c9f985fe3e47bee609ea1aae90994f3e4daad75
dan.shechter@nextsilicon.com <dan.shechter@nextsilicon.com>
2024-05-27 11:03:01 +0300

    Initial commit

---
+++ lua/plugins/community.lua
@@ -0,0 +1,34 @@
+return {
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
+  {
+    "copilot.lua", 
+    opts = { 
+      suggestion = { 
+        keymap = { 
+          accept = "<C-l>", 
+          accept_word = false, 
+          accept_line = false, 
+          next = "<C-.>", 
+          prev = "<C-,>", 
+          dismiss = "<C/>", 
+        }, 
+      }, 
+    }, 
+  },
+}
---
+++ lua/plugins/lazy_loaded.lua
@@ -0,0 +1,6 @@
+return {
+  { "lukas-reineke/headlines.nvim", ft = "markdown", config = true },
+  { "f-person/git-blame.nvim", event = "BufRead" },
+  { "dhruvasagar/vim-table-mode", event = "BufRead", },
+}
+
