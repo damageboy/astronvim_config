4913a6de973199d8204a44ad8beb74e47a7bc174
dan.shechter@nextsilicon.com <dan.shechter@nextsilicon.com>
2025-05-22 11:12:42 +0300

    Remove whitespace

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -17,18 +17,18 @@ return {
     },
   },
   {
-    "copilot.lua", 
-    opts = { 
-      suggestion = { 
-        keymap = { 
-          accept = "<C-l>", 
-          accept_word = false, 
-          accept_line = false, 
-          next = "<C-.>", 
-          prev = "<C-,>", 
-          dismiss = "<C/>", 
-        }, 
-      }, 
-    }, 
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
   },
 }
