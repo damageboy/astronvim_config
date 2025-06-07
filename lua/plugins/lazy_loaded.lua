dd19d72c2054c0856070904bc65bf8fe4b316b06
dan.shechter@nextsilicon.com <dan.shechter@nextsilicon.com>
2025-06-07 09:44:57 +0300

    Update to astronvim v5

--- lua/plugins/lazy_loaded.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "lukas-reineke/headlines.nvim", ft = "markdown", config = true },
-  { "f-person/git-blame.nvim", event = "BufRead" },
-  { "dhruvasagar/vim-table-mode", event = "BufRead", },
-}
-
